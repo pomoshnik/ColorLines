5f3320f08dd1ac1b7b932e412f5bac516e856af0
Игорь <igor_sa@list.ru>
2020-09-07 13:54:50 +0300

    1

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject[] ball = new GameObject[3];
+    private GameObject[,] pole=new GameObject[9, 9];
+    void Start()
+    {
+        StartTable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+            return;
+        }
+    }
+
+    void StartTable() 
+    {
+        int x;
+        int y;
+        int c;
+        for (int i=1;i<6;i++)
+        {
+            x = Random.Range(1, 9);
+            y = Random.Range(1, 9);
+            c = Random.Range(0, 4);
+            if (pole[x, y]!=null)
+            {
+                i--;
+                continue;
+            }
+            var b = Instantiate(ball[c], new Vector3(x-4, y-4,-40f), Quaternion.identity);
+            pole[x, y] = b;
+        }
+    }
+}
---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    private bool start = true;
+    void OnCollisionEnter()
+    {
+        if (start) {
+            var comp = GetComponent<Rigidbody>();
+            comp.velocity = Vector3.zero;
+            comp.angularVelocity = Vector3.zero;
+            comp.isKinematic = true;
+            start = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Hit");
+        var comp = GetComponent<Rigidbody>();
+        comp.isKinematic = false;
+        comp.AddForce(new Vector3(0, 0, -5), ForceMode.Impulse);
+        
+    }
+
+    void Update()
+    {
+        if ((transform.position.z<-3)&&(!start))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -3);
+        }
+    }
+}
